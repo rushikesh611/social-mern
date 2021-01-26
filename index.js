dd39bf959ee9cf67dd4c9963ac77fa95ce7159fe
hrushikesh jadhav <rushi611@gmail.com>
2021-01-26 20:43:10 +0530

    register and login

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 const { MONGODB } = require('./config.js');
-const Post = require('./models/Post');
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
 
-const typeDefs = gql`
-    type Post{
-        id: ID!,
-        body: String!,
-        createdAt: String!,
-        username: String!
-    }
-    type Query{
-        getPosts: [Post]
-    }
-`;
-
-const resolvers={
-    Query:{
-       async getPosts(){
-        try{
-            const posts = await Post.find();
-            return posts;
-        } catch(err){
-            throw new Error(err);
-        }
-        }
-    }
-};
 
 const server = new ApolloServer({
     typeDefs,
@@ -42,4 +18,4 @@ mongoose.connect(MONGODB,{useNewUrlParser: true}).then(()=>{
 })
 
 
-// 39:40 https://www.youtube.com/watch?v=n1mdAPFq2Os&list=WL&index=29w
+// 1:12:50 https://www.youtube.com/watch?v=n1mdAPFq2Os&list=WL&index=29w
