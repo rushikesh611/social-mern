da4d225a0c338790812ab87a3cad04006dc791c5
hrushikesh jadhav <rushi611@gmail.com>
2021-01-28 23:21:36 +0530

    comments and likes backend added

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require('../../models/Post');
 const checkAuth = require('../../utils/check-auth');
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 module.exports={
     Query:{
@@ -30,8 +30,10 @@ module.exports={
         async createPost(_,{body},context){
             const user = checkAuth(context);
             
-            console.log(user);
-            
+            // console.log(user);
+            if(args.body.trim()===''){
+                throw new Error('Post body must not be empty');
+            }
             const newPost = new Post({
                 body,
                 user: user.id,
@@ -40,6 +42,9 @@ module.exports={
             });
             const post = await newPost.save();
             
+            context.pubsub.publish('NEW_POST',{
+                newPost: post
+            })
             return post;
         },
         async deletePost(_,{postId},context){
@@ -55,6 +60,29 @@ module.exports={
             } catch(err){
                 throw new Error(err);
             }
+        },
+        async likePost(_,{postId},context){
+            const {username} = checkAuth(context);
+            const post = await Post.findById(postId);
+            if(post){
+                if(post.likes.find(like => like.username === username)){
+                    //Post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username);    
+                } else {
+                    // Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
+        }
+    },
+    Subsciption:{
+        newPost:{
+            subscribe: (_,__,{pubsub}) => pubsub.asyncIterator('NEW_POST')
         }
     }
 };
